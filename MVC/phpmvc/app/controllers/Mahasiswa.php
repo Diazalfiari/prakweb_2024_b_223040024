9ea0501421596fb5154cd9cffe471f78cad984ae
Diazalfiari <diazalfiari19@gmail.com>
2024-10-20 17:32:57 +0700

    Menyelesaikan video 8 - Database Wrapper

--- MVC/phpmvc/app/controllers/Mahasiswa.php
+++ MVC/phpmvc/app/controllers/Mahasiswa.php
@@ -1,14 +1,21 @@
-<?php 
+<?php
 
-class Mahasiswa extends Controller {
-    public function index() {
-        
-        $data['judul'] = 'Daftar Mahasiswa';
+class Mahasiswa extends Controller
+{
+    public function index()
+    {
+        $data['judul'] = 'Daftar mahasiswa';
         $data['mhs'] = $this->model('Mahasiswa_model')->getAllMahasiswa();
         $this->view('templates/header', $data);
         $this->view('mahasiswa/index', $data);
         $this->view('templates/footer');
-        
     }
-    
+    public function detail($id)
+    {
+        $data['judul'] = 'Detail mahasiswa';
+        $data['mhs'] = $this->model('Mahasiswa_model')->getMahasiswaById($id);
+        $this->view('templates/header', $data);
+        $this->view('mahasiswa/detail', $data);
+        $this->view('templates/footer');
+    }
 }
