c3c30bf301a646af71a62d3e2b0a63906a4243dc
Diazalfiari <diazalfiari19@gmail.com>
2024-10-20 10:20:58 +0700

    Menyelesaikan video 6 - Assets

--- MVC/phpmvc/app/views/About/index.php
+++ MVC/phpmvc/app/views/About/index.php
@@ -1,4 +1,7 @@
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/profil.jpg" alt="Diaz Alfiari Rachmad" width="200" class="rounded-circle shadow">
+    <p>Halo, nama saya <?= $data['nama']; ?>,umur <?= $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?>
+    </p>
 
-
-    <h1>About Me</h1>
-    <p>Halo, nama saya <?= $data['nama']; ?>,umur <?= $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
