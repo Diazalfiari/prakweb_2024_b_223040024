76c1dbad4ebac554df714b325cb1484f343eb940
Diazalfiari <diazalfiari19@gmail.com>
2024-10-20 08:13:53 +0700

    Menyelesaikan video 5 - View

--- MVC/phpmvc/app/controllers/About.php
+++ MVC/phpmvc/app/controllers/About.php
@@ -1,11 +1,24 @@
 <?php 
 
-class About {
-    public function index($nama = 'yuli', $pekerjaan = 'guru') {
-        echo "Hallo, nama saya  $nama, saya adalah seorang $pekerjaan";
+class About extends Controller {
+    public function index($nama = 'yuli', $pekerjaan = 'guru', $umur = 45) {
 
+        
+        $data['nama'] = $nama;
+        $data['umur'] = $umur;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header',  $data);
+
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
+         
     }
     public function page() {
-        echo 'About/page';
+        
+        $data['judul'] = 'Page';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
